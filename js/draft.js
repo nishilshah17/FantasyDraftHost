575b4d04a3943b19b19c39c379a3ef61c072809e
Nishil Shah <nishil.shah17@gmail.com>
2015-07-09 23:14:48 -0400

    starting basics of draft page

--- js/draft.js
+++ js/draft.js
@@ -2,6 +2,52 @@ var draftID;
 
 $(document).ready(function() {
   $('#draftIDSubmit').click(function() {
-    draftID = $('#draftID').val();
+    draftID = parseInt($('#draftID').val());
+    alert(draftID);
+
+    //verify draftID using firebase
+
+    $('#draftID').remove();
+    $('#draftIDSubmit').remove();
+
+    if(draftID > 0) {
+      alert("getting here");
+
+      var currentPick = 0;
+      var teams = [];
+      var owners = [];
+      var rounds;
+
+      //replace the following bs data with firebase later
+      for(var i = 0; i < 10; i++) {
+        teams.push('team '+(i+1));
+        owners.push('owner '+(i+1));
+      }
+      rounds = 17;
+
+      var table = $("<table></table>");
+      table.attr('id','draftTable');
+
+      for(var y = 0; y < rounds; y++) {
+        var row = $("<tr></tr>");
+        row.attr('id','round'+(y+1));
+
+        for(var x = 0; x < teams.length+1; i++) {
+          var cell;
+          if(x == 0) {
+            cell = $('<th>1</th>');
+          } else {
+            currentPick++;
+            cell = $('<th>'+currentPick+'</th>');
+          }
+          row.append(cell);
+        }
+        table.append(row);
+      }
+
+      $('#draftBody').append(table);
+
+    }
+
   });
 });
