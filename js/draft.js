0821a09cc763ac142866a14156cb75038e26034b
Nishil Shah <nishil.shah17@gmail.com>
2015-07-29 23:26:10 -0400

    removing unnecessary stuff

--- js/draft.js
+++ js/draft.js
@@ -512,3 +512,9 @@ function getPositionColor(position) {
 
 function nothing() {
 }
+
+function make_base_auth(user, password) {
+    var tok = user + ':' + password;
+    var hash = btoa(tok);
+    return 'Basic ' + hash;
+}
